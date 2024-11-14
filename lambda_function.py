115264e98e1f886ff8cbb15e32ac0e17632a5591
GGURIMON <ehdrhks105@naver.com>
2024-11-14 17:32:55 +0900

    add s3 upload result

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,9 @@
 import json
 import logging
-from s3 import download_file
+from s3 import download_file, upload_base64_to_s3
 from image_app import image_edit
 import base64
+import random
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -11,16 +12,15 @@ def lambda_handler(event, context):
     try:
         # POST 요청 본문에서 데이터 가져오기
         logger.info(event)
-        prompt = event.get("prompt").replace(" ","")
+        # query_params = event.get("queryStringParameters", {})
+        # edit_task = query_params.get("task", "")
+        edit_task = event.get("task", None)
+        logger.info(edit_task)
+        prompt = event.get("prompt", None)
         logger.info(prompt)
         file_name = event.get("file_name", None)  # 원본 이미지 파일 경로
         logger.info(file_name)
-        if '.' not in file_name:
-            logger.error(f"Invalid file name format: {file_name}")
-            return {
-                "statusCode": 400,
-                "body": json.dumps("Invalid file name format. Please include an extension (e.g., .jpg, .png).")
-            }
+
         masked_name = file_name.split(".")
         masked_image_name = f"{masked_name[0]}_masked.{masked_name[1]}"
         # masked_image_name = body.get("masked_image_name", None)  # 마스킹 이미지 파일 경로
@@ -40,15 +40,15 @@ def lambda_handler(event, context):
             }
 
         # "배경 제거" 조건일 경우 bgrm 기능 실행
-        if "배경제거" in prompt:
+        if edit_task == "bgrm":
             response = image_edit(original_image_base64, task="BACKGROUND_REMOVAL")
-        elif "컨디셔닝" in prompt:
+        elif edit_task == "conditioning":
             response = image_edit(original_image_base64, prompt, task = "IMAGE_CONDITIONING")
-        elif "아웃페인팅" in prompt:
+        elif edit_task == "outpaint":
             if not masked_image_name:
                 return {
                     "statusCode": 400,
-                    "body": json.dumps("Missing 'masked_image_name' for inpainting task.")
+                    "body": json.dumps("Missing 'masked_image_name' for outpainting task.")
                 }
 
             # 마스킹 이미지 다운로드 및 Base64 인코딩
@@ -76,8 +76,19 @@ def lambda_handler(event, context):
                 }
 
             # 인페인팅 기능 실행
+            logger.info(prompt)
             response = image_edit(original_image_base64, mask_image_base64, prompt)
             logger.info(response)
+            logger.info(type(response))
+            # result = base64.b64encode(response)
+            # logger.info(result)
+
+        random_number = random.randint(1, 10000)
+        try:
+            upload_base64_to_s3(response, f"output_image_{random_number}.jpg")
+        except:
+            pass
+            
         return {
             "statusCode": 200,
             "body": json.dumps(response),
